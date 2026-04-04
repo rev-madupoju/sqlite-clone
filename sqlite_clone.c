0697b8b8e8516fee39847a0422d02f8316ddc69a
Revanth Madupoju <revanthmadupoju@gmail.com>
2026-04-04 13:19:34 +0500

    fix: extra size alloc for Row(struct) members
    C Strings usually end with a null character, so that when printed, the
    output looks clean

--- sqlite_clone.c
+++ sqlite_clone.c
@@ -59,8 +59,8 @@ typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
 // Row struct
 typedef struct {
   uint32_t id;
-  char username[COLUMN_USERNAME_SIZE];
-  char email[COLUMN_EMAIL_SIZE];
+  char username[COLUMN_USERNAME_SIZE + 1];
+  char email[COLUMN_EMAIL_SIZE + 1];
 } Row;
 
 // Statement struct
